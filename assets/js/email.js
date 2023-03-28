f9d01b4d255feb21b920bd1b30bb39341eeb5705
Squirtle22 <djkeel@live.co.uk>
2023-03-28 23:04:22 +0100

    building style of contact page

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    document.getElementById('contact_form').addEventListener('submit', function(event) {
         event.preventDefault();
         // these IDs from the previous steps
         emailjs.sendForm('service_q8gxg28', 'template_ssdym4a', this)
